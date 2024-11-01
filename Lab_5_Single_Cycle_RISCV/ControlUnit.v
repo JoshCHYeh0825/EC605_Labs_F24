28d3912e44ad40a37906ce5f76cbb4bb71210b76
Josh CH Yeh <jy0825@bu.edu>
2024-11-01 17:38:25 -0400

    Add files via upload

---
+++ Lab_5_Single_Cycle_RISCV/ControlUnit.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module Control_Unit (
+    input clk,
+    input reset,
+    output reg [31:0] pc_out
+);  
+
+   always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            pc_out <= 0; // Reset to address of 1st instruction
+        end else begin
+            pc_out <= pc_out + 1; // Increment by 1
+        end
+    end
+
+endmodule
