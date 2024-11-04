79b4441b6379ad60526b713d0cd1f09ff89dd852
Josh CH Yeh <jy0825@bu.edu>
2024-11-04 17:42:44 -0500

    Update BranchControl.v

--- Lab_5_Single_Cycle_RISCV/BranchControl.v
+++ Lab_5_Single_Cycle_RISCV/BranchControl.v
@@ -2,32 +2,30 @@
 
 module Branch_Control (
     
-    input branch,           // Branch signal from control unit
-    input [2:0] func3,      // Branch type code (e.g., BEQ, BLT)
-    input zero,             // Zero flag from ALU
-    input negative,         // Sign flag from ALU
-    input [31:0] pc,        // Current PC value
-    input [31:0] imm,       // Immediate offset for branch
-    output reg branch_taken,       // Branch condition met
-    output reg [31:0] branch_target // Branch address if taken
+    input branch, // Branch signal from control unit
+    input [2:0] FUNCT3, // Branch type code (e.g., BEQ, BLT)
+    input zero_flag, // Zero flag from ALU
+    input n, // Sign flag from ALU
+    input [31:0] pc, // Current PC value
+    input [31:0] imm, // Immediate offset for branch
+    output reg branch_taken, // Branch condition met
+    output reg [31:0] pc_out // PC address if branch is true
 );
     always @(*) begin
         // Default values
         branch_taken = 1'b0;
-        branch_target = pc + 4;
+        pc_out = pc + 1;
 
         if (branch) begin
-            case (func3)
-                3'b000: branch_taken = zero;                   // BEQ
-                3'b001: branch_taken = ~zero;                  // BNE
-                3'b100: branch_taken = negative;               // BLT
-                3'b101: branch_taken = ~negative;              // BGE
+            case (FUNCT3)
+                3'b000: branch_taken = zero_flag; // BEQ
+                3'b001: branch_taken = n;         // BLT
                 default: branch_taken = 1'b0;
             endcase
 
             // Set branch target address if branch is taken
             if (branch_taken) begin
-                branch_target = pc + imm; // Adjust target address by immediate offset
+                pc_out = pc + (imm << 1); // Adjust target address by immediate offset
             end
         end
     end
